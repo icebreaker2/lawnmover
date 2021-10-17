6f9324036441807a84e9a3c20706ef8c9773e750
Hendrik Kahlen <hendrikkahlen.hk@gmail.com>
2021-10-17 17:36:32 +0200

    LAWN-05 : Fix spi slave to handle rx_buffer and call spi_commands utils
    Signed-off-by: Hendrik Kahlen <hendrikkahlen.hk@gmail.com>

--- lawnmover_engines_control_unit/spi_slave.cpp
+++ lawnmover_engines_control_unit/spi_slave.cpp
@@ -4,15 +4,23 @@
 #include <serial_logger.h>
 #include <spi_commands.h>
 
-SpiSlave::SpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin) {
-    AddArduinoSpiSlave(sck_pin, miso_pin, mosi_pin, ss_pin);
-}
+volatile int pos = 0;
+volatile uint8_t id_bytes [COMMAND_FRAME_ID_SIZE];
+volatile uint8_t value_bytes [COMMAND_FRAME_VALUE_SIZE];
 
-SpiSlave::~SpiSlave() {
-    //SPI.end();
-}
+const int engine_commands_size = ENGINE_COMMANDS * COMMAND_FRAME_SIZE;
+int counter = 0;
+int engine_commands_iterator = 0;
+uint8_t rx_buffer[engine_commands_size];
+uint8_t tx_buffer[engine_commands_size];
 
-void SpiSlave::AddArduinoSpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin) {
+bool (*left_wheel_steering_command_)(int16_t) = nullptr;
+bool (*right_wheel_steering_command_)(int16_t) = nullptr;
+bool (*motor_speed_command_)(int16_t) = nullptr;
+
+SpiSlave::SpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin,
+                   bool (*left_wheel_steering_command)(int16_t), bool (*right_wheel_steering_command)(int16_t),
+                   bool (*motor_speed_command)(int16_t)) {
     pinMode(sck_pin, INPUT);
     pinMode(mosi_pin, INPUT);
     pinMode(miso_pin, OUTPUT);  // (only if bidirectional mode needed)
@@ -36,78 +44,76 @@ void SpiSlave::AddArduinoSpiSlave(const int sck_pin, const int miso_pin, const i
     SPCR |= bit(SPE);
     // turn on interrupts
     SPCR |= bit(SPIE);
+
+    left_wheel_steering_command_ = left_wheel_steering_command;
+    right_wheel_steering_command_ = right_wheel_steering_command;
+    motor_speed_command_ = motor_speed_command;
 }
 
-// SPI interrupt routine
-volatile int pos = 0;
-uint8_t id_bytes [2];
-uint8_t value_bytes [4];
-
-int counter = 0;
-uint8_t rx_buffer[27];
-uint8_t tx_buffer[27];
+/**
+    SPI interrupt routine
+    Note: Printing consumes too much time. Slave does not respond in time.
+*/
 ISR (SPI_STC_vect) {
-    // TODO Printing consumes too much time. Slave does not respond in time.
     const uint8_t c = SPDR;
     //SerialLogger::debug("Received: %d", c);
     rx_buffer[counter] = c;
     tx_buffer[counter] = c;
-    if (pos == 0 || pos == 1) {
+    if (pos < COMMAND_FRAME_ID_SIZE) {
         id_bytes[pos] = c;
-        //Serial.println(pos);
-        //Serial.print("Response: ");
-        //Serial.println(c, HEX);
-
         SPDR = c;
-        tx_buffer[pos] = c;
-    } else if (pos < 6) {
-        value_bytes[pos - 2] = c;
-        //Serial.println(pos);
-        //Serial.print("Response: ");
-        //Serial.println(c, HEX);
+        pos++;
+    } else if (pos < COMMAND_FRAME_ID_SIZE + COMMAND_FRAME_VALUE_SIZE) {
+        value_bytes[pos - COMMAND_FRAME_ID_SIZE] = c;
         SPDR = c;
-        tx_buffer[pos] = c;
-    } else if (pos < 8) {
+        pos++;
+    } else if (pos < COMMAND_FRAME_SIZE - COMMAND_SPI_RX_OFFSET) {
         if (c != 0xFF) {
             SerialLogger::warn("Received bad ack id request id byte %c", c);
         }
-        SPDR = id_bytes[pos % 2];
-        tx_buffer[pos] = id_bytes[pos % 2];
-        tx_buffer[counter] = id_bytes[pos % 2];
-        //Serial.println(pos);
-        //Serial.print("Response: ");
-        //Serial.println(id_bytes[pos % 2], HEX);
+        SPDR = id_bytes[pos % COMMAND_FRAME_ID_SIZE];
+        tx_buffer[counter] = id_bytes[pos % COMMAND_FRAME_ID_SIZE];
+        pos++;
     } else {
-        //Serial.println(pos);
-        //Serial.print("Response: ");
-        //Serial.println(0, HEX);
-        // For the master to receive the nth byte we need to send a n+1 byte
         SPDR = 0;
         tx_buffer[counter] = 0;
-    }
-
-    counter = (counter + 1) % 27;
-
-    if (pos == 8) {
         pos = 0;
-    } else {
-        pos++;
+
+        const int16_t id = SpiCommands::slave_interpret_command_id(id_bytes);
+        const int tx_rx_offset = engine_commands_iterator * COMMAND_FRAME_SIZE;
+        const bool valid_command = SpiCommands::slave_interpret_command(id, rx_buffer +  tx_rx_offset + COMMAND_FRAME_ID_SIZE,
+                                   tx_buffer + tx_rx_offset + COMMAND_FRAME_ID_SIZE, left_wheel_steering_command_,
+                                   right_wheel_steering_command_, motor_speed_command_);
+        if (!valid_command) {
+            SerialLogger::warn("Did not receive valid command. Cannot interpret value");
+        }
+        engine_commands_iterator = (engine_commands_iterator + 1) % ENGINE_COMMANDS;
     }
+
+    counter = (counter + 1) % engine_commands_size;
 
 }  // end of interrupt service routine (ISR) SPI_STC_vect
 
 void SpiSlave::addSlavePrinting(Timer<> &timer, const int interval) {
     timer.every(interval, [](void*) -> bool {
-        Serial.print("Received: ");
-        for (int i = 0; i < 27; i++) {
+        Serial.print("RxBufferInput:");
+        for (long i = 0; i < engine_commands_size; i += 1) {
+            if (i % COMMAND_FRAME_SIZE == 0) {
+                Serial.print(" ");
+            }
             Serial.print(rx_buffer[i], HEX);
         }
-        Serial.println("");
-        Serial.print("Send: ");
-        for (int i = 0; i < 27; i++) {
+        Serial.println();
+
+
+        Serial.print("TxBufferInput:");
+        for (long i = 0; i < engine_commands_size; i += 1) {
+            if (i % COMMAND_FRAME_SIZE == 0) {
+                Serial.print(" ");
+            }
             Serial.print(tx_buffer[i], HEX);
         }
-        Serial.println("");
+        Serial.println();
         return true; // to repeat the action - false to stop
     });
 }
--- lawnmover_engines_control_unit/spi_slave.h
+++ lawnmover_engines_control_unit/spi_slave.h
@@ -2,11 +2,14 @@
 
 class SpiSlave {
     public:
-        SpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin);
-        ~SpiSlave();
+        SpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin,
+                 bool (*leftWheelSteeringCommand)(int16_t), bool (*rightWheelSteeringCommand)(int16_t),
+                 bool (*motorSpeedCommand)(int16_t));
+        ~SpiSlave() {
+            // nothing to do...
+        };
 
         void addSlavePrinting(Timer<> &timer, const int interval);
 
     private:
-        void AddArduinoSpiSlave(const int sck_pin, const int miso_pin, const int mosi_pin, const int ss_pin);
 };
