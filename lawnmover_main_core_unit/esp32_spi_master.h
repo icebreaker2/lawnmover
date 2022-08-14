e4c07ee432f1ff59172eb09db1eb6bdf369a42e1
Hendrik Kahlen <hendrikkahlen.hk@gmail.com>
2022-08-14 17:54:56 +0200

    01 : Apply new SpiSlave design to master and ino
    Signed-off-by: Hendrik Kahlen <hendrikkahlen.hk@gmail.com>

--- lawnmover_main_core_unit/esp32_spi_master.h
+++ lawnmover_main_core_unit/esp32_spi_master.h
@@ -5,51 +5,51 @@
 #include <esp32-hal-spi.h>
 // See https://github.com/espressif/arduino-esp32/blob/master/libraries/SPI/src/SPI.h
 #include <SPI.h>
-#include "spi_slave_handler.h"
-#include "spi_slave_controller.h"
+#include "master_spi_slave.h"
 
 #define MAX_SLAVES 5
 
 class Esp32SpiMaster {
-    public:
-        Esp32SpiMaster(const int clock_pin, const int miso_pin, const int mosi_pin, void (*error_callback)(),
-                       const long frequency = 2000000, const int dma_channel = 1, const uint8_t spi_mode = SPI_MODE0,
-                       const int tx_rx_buffer_size = 60, const int chunk_size = 1);
-        ~Esp32SpiMaster();
+  public:
+    static int take_free_id();
 
-        void addSlave(const int slave_pin, const int slave_power_pin, const int slave_boot_delay, const int interval,
-                      const int inter_transaction_delay_microseconds, const long clock_divider, Timer<> &timer,
-                      SpiSlaveController *spiSlaveController);
+    static bool put_free_id(const int id);
 
-    private:
-        const int k_clock_pin;
-        const int k_miso_pin;
-        const int k_mosi_pin;
-        const int k_frequency;
-        const int k_dma_channel;
-        const uint8_t k_spi_mode;
+    Esp32SpiMaster(const int clock_pin, const int miso_pin, const int mosi_pin, void (*error_callback)(),
+                   const long frequency = 2000000, const int dma_channel = 1, const uint8_t spi_mode = SPI_MODE0,
+                   const int tx_rx_buffer_size = 60, const int chunk_size = 1, const int inter_transaction_delay_microseconds = 10);
+    ~Esp32SpiMaster();
 
-        void (*error_callback_)();
-        volatile bool shutdown_ = false;
-        int max_intervall_ = 0;
+    void put_slave(MasterSpiSlave *spi_slave);
 
-        const int k_chunk_size;
-        const int k_tx_rx_buffer_size;
-        uint8_t* spi_master_rx_buf_;
+    SpiSlaveHandler *get_handler(const int slave_pin);
 
-        static SpiSlaveHandler *_slave_handlers[];
-        static int free_ids[];
-        static int get_free_id();
-        static bool add_free_id(const int id);
+    void schedule(const int interval, Timer<> &timer);
 
-        SpiSlaveHandler *setup_slave(const int slave_pin, const int slave_power_pin, const int slave_boot_delay,
-                                     const int interval, const int slave_id);
-        static void restart_slave(const int slave_pin, const int slave_power_pin, const int slave_boot_delay, const int slave_id);
-        static void tear_down_slave(const int slave_pin, const int slave_power_pin, const int slave_id);
-        static void add_timer(const int slave_id, const int slave_pin, const int slave_power_pin, const int slave_boot_delay,
-                              const int interval, const int inter_transaction_delay_microseconds, const long clock_divider,
-                              Timer<> &timer, SpiSlaveController *spiSlaveController, const int chunk_size, uint8_t* rx_buffer,
-                              int max_tx_rx_buffer_size, void (*error_callback)(), volatile bool &shutdown, SpiSlaveHandler *master);
+  private:
+
+    static void schedule_internal(const int interval, Timer<> &timer, const int chunk_size, uint8_t* rx_buffer, int max_tx_rx_buffer_size, void (*error_callback)(), volatile bool &shutdown,
+                                  MasterSpiSlave **slaves, const int inter_transaction_delay_microseconds, volatile int &slave_cursor, const int registered_slaves);
+
+    static MasterSpiSlave *_slaves[];
+    static int _registered_slaves;
+    static volatile int _slave_cursor;
+    static int free_ids[];
+
+    const int k_clock_pin;
+    const int k_miso_pin;
+    const int k_mosi_pin;
+    const int k_frequency;
+    const int k_dma_channel;
+    const uint8_t k_spi_mode;
+    const int k_inter_transaction_delay_microseconds;
+
+    void (*error_callback_)();
+    volatile bool shutdown_ = false;
+
+    const int k_chunk_size;
+    const int k_tx_rx_buffer_size;
+    uint8_t* spi_master_rx_buf_;
 };
 
 #endif // ESP32_SPI_MASTER_H
--- lawnmover_main_core_unit/lawnmover_main_core_unit.ino
+++ lawnmover_main_core_unit/lawnmover_main_core_unit.ino
@@ -1,66 +1,75 @@
 #include <serial_logger.h>
-#include "esp32_ps4_controller.h"
+#include "ESP32_PS4_Controller.h"
 #include "esp32_spi_master.h"
-#include "engine_controller.h"
-#include "object_detection_controller.h"
+#include "engine_slave.h"
+#include "object_detection_Slave.h"
 
 // General SPI settings
-const int MOSI_PIN_GREEN  = 23;
-const int MISO_PIN_YELLOW =  19;
-const int SCK_PIN_ORANGE =  18;
+const int MOSI_PIN_GREEN = 23;
+const int MISO_PIN_YELLOW = 19;
+const int SCK_PIN_ORANGE = 18;
 const long frequency = 2000000;
 const long clock_divide = SPI_CLOCK_DIV8;
+const int INTER_TRANSACTION_DELAY_MICROSECONDS = 10;
 
 // Engine SPI slave settings
 const int ENGINE_CONTROL_SS_PIN_BLUE = 5;
-const int ENGINE_POWER_SUPPLY_PIN = 13;
+const int ENGINE_RESTART_PIN_PIN = 13;
 const int ENGINE_POWER_BOOT_DELAY = 1000;
-const int ENGINE_INTER_TRANSACTION_DELAY_MICROSECONDS = 10;
-const int engine_control_unit_interval = 300;
 
 // Object Detection SPI slave settings
 const int OBJECT_DETECTION_CONTROL_SS_PIN_BROWN = 12;
-const int OBJECT_DETECTION_POWER_SUPPLY_PIN = 14;
+const int OBJECT_DETECTION_RESTART_PIN_PIN = 14;
 const int OBJECT_DETECTION_POWER_BOOT_DELAY = 1000;
-const int OBJECT_DETECTION_INTER_TRANSACTION_DELAY_MICROSECONDS = 10;
-const int object_detection_control_unit_interval = 150;
+
+const int spi_schedule_next_slave_commands_intervall = 150;
 
 // General processing + PS4 (Bluetooth) settings
 auto _timer = timer_create_default();
 const char *masterMac = "ac:89:95:b8:7f:be";
 ESP32_PS4_Controller *esp32Ps4Ctrl = nullptr;
 Esp32SpiMaster *esp32_spi_master = nullptr;
-EngineController *engine_controller;
-ObjectDetectionController *object_detection_controller;
+EngineSlave *engine_Slave;
+ObjectDetectionSlave *object_detection_Slave;
 
 void re_setup_spi_communication() {
-    // delete the master will tear down all slaves (inklusive their power supply)
-    SerialLogger::info("Shutting down all previous slaves");
-    delete esp32_spi_master;
-    SerialLogger::info("(Re)Setting up all slaves");
-    esp32_spi_master = new Esp32SpiMaster(SCK_PIN_ORANGE, MISO_PIN_YELLOW, MOSI_PIN_GREEN, &re_setup_spi_communication,
-                                          frequency);
-    esp32_spi_master->addSlave(ENGINE_CONTROL_SS_PIN_BLUE, ENGINE_POWER_SUPPLY_PIN,
-                               ENGINE_POWER_BOOT_DELAY, ENGINE_INTER_TRANSACTION_DELAY_MICROSECONDS,
-                               engine_control_unit_interval, clock_divide, _timer, engine_controller);
-    esp32_spi_master->addSlave(OBJECT_DETECTION_CONTROL_SS_PIN_BROWN, OBJECT_DETECTION_POWER_SUPPLY_PIN,
-                               OBJECT_DETECTION_POWER_BOOT_DELAY,
-                               OBJECT_DETECTION_INTER_TRANSACTION_DELAY_MICROSECONDS,
-                               object_detection_control_unit_interval, clock_divide, _timer,
-                               object_detection_controller);
+	// delete the master will tear down all slaves (inclusive their power supply) put into master
+	SerialLogger::info("Shutting down all previous slaves");
+	delete esp32_spi_master;
+	SerialLogger::info("(Re)Setting up all slaves");
+	esp32_spi_master = new Esp32SpiMaster(SCK_PIN_ORANGE, MISO_PIN_YELLOW, MOSI_PIN_GREEN, &re_setup_spi_communication,
+										  frequency, INTER_TRANSACTION_DELAY_MICROSECONDS);
+
+	const int slave_id = Esp32SpiMaster::take_free_id();
+	if (slave_id >= 0) {
+        SpiSlaveHandler *spi_slave_handler = esp32_spi_master->get_handler(ENGINE_CONTROL_SS_PIN_BLUE);
+		EngineSlave *spi_slave = new EngineSlave(spi_slave_handler, slave_id, ENGINE_CONTROL_SS_PIN_BLUE,
+												 ENGINE_RESTART_PIN_PIN, ENGINE_POWER_BOOT_DELAY, esp32Ps4Ctrl);
+		esp32_spi_master->put_slave(spi_slave);
+	} else {
+		SerialLogger::error("Cannot add a new slave to. Got no free id from Esp32SpiMaster");
+	}
+
+
+//  const int slave_id = Esp32SpiMaster::take_free_id();
+//  if (slave_id >= 0) {
+//      TODO create object detection slave
+//  } else {
+//    SerialLogger::error("Cannot add a new slave to. Got no free id from Esp32SpiMaster");
+//    return nullptr;
+//  }
+    esp32_spi_master->schedule(spi_schedule_next_slave_commands_intervall, _timer);
 }
 
 void setup() {
-    SerialLogger::init(9600, SerialLogger::LOG_LEVEL::INFO);
-    esp32Ps4Ctrl = new ESP32_PS4_Controller(masterMac, _timer);
-    engine_controller = new EngineController(esp32Ps4Ctrl);
-    object_detection_controller = new ObjectDetectionController();
+	SerialLogger::init(9600, SerialLogger::LOG_LEVEL::DEBUG);
+	esp32Ps4Ctrl = new ESP32_PS4_Controller(masterMac, _timer);
 
-    re_setup_spi_communication();
+	re_setup_spi_communication();
 }
 
 
 void loop() {
-    // tick timers
-    auto ticks = _timer.tick();
+	// tick timers
+	auto ticks = _timer.tick();
 }
